56236202266f0602b17e2552dac656de4af9d35d
joq62 <joakim.leche@gmail.com>
2020-12-28 00:13:46 +0100

    update

--- src/service.erl
+++ src/service.erl
@@ -11,24 +11,32 @@
 %% --------------------------------------------------------------------
 
 %-compile(export_all).
--export([create/6
+-export([create/3
 	]).
 
+create(ServiceSpecId,VmDir,Vm)->
+    Result=case if_db:call(db_service_def,read,[ServiceSpecId]) of
+	       []->
+		   {error,[eexists,ServiceSpecId]};
+	       [{ServiceSpecId,ServiceId,ServiceVsn,StartCmd,GitPath}]->
+		   create(ServiceId,ServiceVsn,Vm,VmDir,StartCmd,GitPath)
+	   end,
+    Result.
 
-create(Vm,VmDir,AppId,Vsn,{M,F,A},GitPath)->
-    AppDir=string:concat(AppId,misc_cmn:vsn_to_string(Vsn)),
-    GitDest=filename:join(VmDir,AppDir),
-    CodePath=filename:join([VmDir,AppDir,"ebin"]),
-    [AppModule]=A,
+create(ServiceId,ServiceVsn,Vm,VmDir,{application,start,A},GitPath)->
+    ServiceDir=string:concat(ServiceId,misc_cmn:vsn_to_string(ServiceVsn)),
+    GitDest=filename:join(VmDir,ServiceDir),
+    CodePath=filename:join([VmDir,ServiceDir,"ebin"]),
+    [ServiceModule]=A,
     true=vm:vm_started(Vm),
     rpc:call(Vm,file,del_dir_r,[GitDest],3000),
     rpc:call(Vm,os,cmd,["git clone "++GitPath++" "++GitDest],10*1000),
     true=rpc:call(Vm,code,add_patha,[CodePath],3000),
-    Result=case rpc:call(Vm,M,F,A,3000) of
+    Result=case rpc:call(Vm,application,start,A,3000) of
 	       ok->
-		   case rpc:call(Vm,AppModule,ping,[],3000) of
-		       {pong,_,AppModule}->
-			   ok;
+		   case rpc:call(Vm,ServiceModule,ping,[],3000) of
+		       {pong,_,ServiceModule}->
+			   {ok,ServiceId,ServiceVsn};
 		       Reason->
 			   {error,[Reason,?MODULE,?LINE]}
 		   end;
